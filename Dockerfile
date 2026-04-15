1ea21f96ed9cf449a63c11a7b74bbb74fef0f881
Sheran Gunasekera <sheran@zensay.com>
2026-04-16 00:13:28 +0800

    remove unbound, split into 3 parallel

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,8 @@
 FROM golang:alpine
 
-RUN apk update && apk add --no-cache bind-tools unbound ca-certificates tzdata && update-ca-certificates
+RUN apk update && apk add --no-cache bind-tools ca-certificates tzdata && update-ca-certificates
 RUN go install github.com/OJ/gobuster/v3@latest
 ENV TZ=Asia/Singapore
-COPY pi-hole.conf /etc/unbound/unbound.conf
-COPY root.hints /etc/unbound/root.hints
 COPY runner.sh /usr/bin/runner.sh
 ADD https://raw.githubusercontent.com/sheran/SecLists/master/Discovery/DNS/subdomains-top1million-110000.txt /usr/lib/dnsscan/wordlist.txt
-EXPOSE 5335/tcp
-EXPOSE 5335/udp
 ENTRYPOINT [ "runner.sh" ]
--- runner.sh
+++ runner.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
-unbound
-gobuster dns --domain $TLD --quiet --wordlist /usr/lib/dnsscan/wordlist.txt --resolver 127.0.0.1:5335 --threads 15 --delay 10ms --no-color --output /out/${TLD}.hosts.txt "$@"
+set -e
+
+WORDLIST=${WORDLIST:-/usr/lib/dnsscan/wordlist.txt}
+OUT=/out/${TLD}.hosts.txt
+TMPDIR=$(mktemp -d)
+trap 'rm -rf "$TMPDIR"' EXIT
+
+awk -v d="$TMPDIR" '{ print > d"/w" (NR%3) }' "$WORDLIST"
+
+i=0
+pids=""
+for r in 1.1.1.1 8.8.8.8 8.8.4.4; do
+    gobuster dns --domain "$TLD" --quiet --wordlist "$TMPDIR/w$i" \
+        --resolver "$r:53" --threads 15 --delay 10ms --no-color \
+        --output "$TMPDIR/out$i" "$@" &
+    pids="$pids $!"
+    i=$((i+1))
+done
+
+rc=0
+for p in $pids; do wait "$p" || rc=$?; done
+
+cat "$TMPDIR"/out* 2>/dev/null | sort -u > "$OUT"
+exit $rc
