7ec30c25b210c4989881ba1c54f7a2c66cb0f615
Sheran Gunasekera <sheran@zensay.com>
2026-04-15 23:58:47 +0800

    fix typo in param

--- runner.sh
+++ runner.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 unbound
-gobuster dns --domain $TLD --quiet --wordlist /usr/lib/dnsscan/wordlist.txt --resolver 127.0.0.1:5335 --threads 15 --delay 10ms --no-color --ooutput /out/${TLD}.hosts.txt "$@"
+gobuster dns --domain $TLD --quiet --wordlist /usr/lib/dnsscan/wordlist.txt --resolver 127.0.0.1:5335 --threads 15 --delay 10ms --no-color --output /out/${TLD}.hosts.txt "$@"
